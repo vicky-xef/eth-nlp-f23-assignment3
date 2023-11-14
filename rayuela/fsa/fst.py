3ba973062941d4d443d9b3a958ca7763a99996e0
vicky-xef <vicky.xefteri@gmail.com>
2023-11-14 18:24:31 +0100

    different file for epsilon filter

--- rayuela/fsa/fst.py
+++ rayuela/fsa/fst.py
@@ -187,38 +187,4 @@ class FST(FSA):
 
         return T
 
-    def get_epsilon_filter(R, Sigma):
-        """Returns the epsilon filtered FST required for the correct composition of WFSTs
-        with epsilon transitions.
-        
-        Returns:
-        FST: The 3-state epsilon filtered WFST.
-        """
-        
-        F = FST(R)
-        
-        # 0 ->
-        for a in Sigma:
-            F.add_arc(State(0), a, a, State(0), R.one)
-        F.add_arc(State(0), ε_2, ε_1, State(0), R.one)
-        F.add_arc(State(0), ε_1, ε_1, State(1), R.one)
-        F.add_arc(State(0), ε_2, ε_2, State(2), R.one)
-        
-        # 1 ->
-        for a in Sigma:
-            F.add_arc(State(1), a, a, State(0), R.one)
-        F.add_arc(State(1), ε_1, ε_1, State(1), R.one)
-        
-        # 2 ->
-        for a in Sigma:
-            F.add_arc(State(2), a, a, State(0), R.one)
-        F.add_arc(State(2), ε_2, ε_2, State(2), R.one)
-        
-        F.set_I(State(0), R.one)
-        F.set_F(State(0), R.one)
-        F.set_F(State(1), R.one)
-        F.set_F(State(2), R.one)
-        
-        return F
-
     
